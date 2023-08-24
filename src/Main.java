c9683f024feeb2148222c1206784113f40004e68
ayou6 <ayouxb.abbas@gmail.com>
2023-08-24 15:56:10 +0400

    inProgress

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.security.SecureRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        String strng1 = "abb";
+        String strng2= "xyy";
+
+        if (strng1.length() == strng2.length()) {
+             strng1.toLowerCase();
+             strng2.toLowerCase();
+
+            
+            
+        } else {
+            System.out.println("its not isomorphic.");
+        }
+    }
+}
+
+
+
+
+
+    }
+}
