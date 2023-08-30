19a65bd00b32b2786b5a8498d775b9969361e7de
ayou6 <ayouxb.abbas@gmail.com>
2023-08-30 11:53:20 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,43 @@
-import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         String strng1 = "abb";
-        String strng2= "xyy";
+        String strng2 = "yyy";
 
         if (strng1.length() == strng2.length()) {
-             strng1.toLowerCase();
-             strng2.toLowerCase();
+            strng1 = strng1.toLowerCase();
+            strng2 = strng2.toLowerCase();
 
-            
-            
+            Map<Character, Character> map = new HashMap<>();
+            boolean isIsomorphic = true;
+
+            for (int i = 0; i < strng1.length(); i++) {
+                char char1 = strng1.charAt(i);
+                char char2 = strng2.charAt(i);
+
+                if (map.containsKey(char1)) {
+                    if (map.get(char1) != char2) {
+                        isIsomorphic = false;
+                        break;
+                    }
+                } else {
+                    if (map.containsValue(char2)) {
+                        isIsomorphic = false;
+                        break;
+                    }
+                    map.put(char1, char2);
+                }
+            }
+
+            if (isIsomorphic) {
+                System.out.println("The strings are isomorphic.");
+            } else {
+                System.out.println("The strings are not isomorphic.");
+            }
         } else {
-            System.out.println("its not isomorphic.");
+            System.out.println("The strings are not isomorphic.");
         }
-    }
-}
-
-
-
-
-
     }
 }
